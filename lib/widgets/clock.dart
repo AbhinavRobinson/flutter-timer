6594923a4380f76fb584c96c4965fa6348a39a84
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-05 20:04:49 +0530

    (feat): clock soft#2

--- lib/widgets/clock.dart
+++ lib/widgets/clock.dart
@@ -19,34 +19,36 @@ class _ClockState extends State<Clock> {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
               colors: [
+                Color.fromARGB(0, 56, 67, 81),
                 Color.fromARGB(25, 56, 67, 81),
-                Color.fromARGB(75, 56, 67, 81),
                 Color.fromARGB(200, 56, 67, 81),
                 Color.fromARGB(255, 56, 67, 81),
               ],
               stops: [
                 0.1,
-                0.2,
-                0.9,
+                0.25,
+                0.75,
                 1,
               ]),
           boxShadow: [
             BoxShadow(
-              color: Color.fromRGBO(0, 0, 0, 0.2),
-              blurRadius: 20.0,
-              offset: Offset(4.0, 4.0),
-            ),
-            BoxShadow(
-              color: Color.fromRGBO(56, 67, 81, 0.3),
+              color: Color.fromRGBO(56, 67, 81, 1),
               blurRadius: 10.0,
               offset: Offset(-4.0, -4.0),
             ),
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.3),
+              blurRadius: 20.0,
+              offset: Offset(4.0, 4.0),
+            ),
           ],
+          border:
+              Border.all(color: Color.fromARGB(255, 56, 67, 81), width: 0.5),
           borderRadius: BorderRadius.circular(300.0),
         ),
         child: Center(
           child: Text(
-            'hello',
+            '25',
             style: TextStyle(
               color: Colors.white,
             ),
