6f6eecfd5e58ec31e7452af21686789bd82d9059
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-05 17:33:16 +0530

    (feat): home_screen bg

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_timer/widgets/top_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          TopBar(),
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(56, 67, 81, 1),
+              Color.fromRGBO(35, 46, 58, 1),
+            ]),
       ),
-      backgroundColor: Color.fromRGBO(33, 33, 45, 1),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          centerTitle: true,
+          toolbarHeight: 150,
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Let\'s Start!',
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w300,
+              color: Color.fromRGBO(123, 137, 154, 1),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
