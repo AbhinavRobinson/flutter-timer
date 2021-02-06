7eff555f3a7a9bb5de701174b01b7624146f141d
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-06 17:10:57 +0530

    (feat): add icon to button

--- lib/widgets/button_timer.dart
+++ lib/widgets/button_timer.dart
@@ -1,5 +1,6 @@
 import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ButtonTimer extends StatelessWidget {
@@ -25,13 +26,19 @@ class ButtonTimer extends StatelessWidget {
             SizedBox(
               height: 10.0,
             ),
-            ClayContainer(
-              color: Theme.of(context).primaryColor,
-              height: 75.0,
-              width: 75.0,
-              // emboss: true,
-              depth: 10,
-              spread: 10,
+            GestureDetector(
+              onTap: () {},
+              child: ClayContainer(
+                color: Theme.of(context).primaryColor,
+                height: 75.0,
+                width: 75.0,
+                // emboss: true,
+                depth: 10,
+                spread: 10,
+                child: Center(
+                  child: FaIcon(FontAwesomeIcons.play, size: 42),
+                ),
+              ),
             ),
           ],
         ),
