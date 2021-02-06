b4a3cc84b946c12b22f45956555700d384ed5383
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-06 16:33:42 +0530

    (feat): add buttons

---
+++ lib/widgets/button_timer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ButtonTimer extends StatelessWidget {
+  const ButtonTimer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Center(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
